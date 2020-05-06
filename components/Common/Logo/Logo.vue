782e046fda373af4ca740a7ae076fbbf6e755a2c
ali pamenary <pamenary@gmail.com>
2020-05-06 10:31:41 +0430

    first style in the file

--- components/Common/Logo/Logo.vue
+++ components/Common/Logo/Logo.vue
@@ -1,10 +1,10 @@
-<template>
-  <img src="/images/logo.png" />
-</template>
-
 <style lang="scss" scoped>
 img {
   height: 45px;
   width: auto;
 }
 </style>
+
+<template>
+  <img src="/images/logo.png" />
+</template>
--- components/Layout/Desktop/HeaderDesktop/HeaderDesktop.vue
+++ components/Layout/Desktop/HeaderDesktop/HeaderDesktop.vue
@@ -1,3 +1,24 @@
+<style lang="scss" scoped>
+header {
+  line-height: 94px;
+  height: 94px;
+  .left-header {
+    ::v-deep {
+      .v-btn {
+        line-height: 42px;
+        height: 42px;
+        a {
+          color: #fff;
+        }
+      }
+    }
+    .auth-link {
+      color: #212121;
+    }
+  }
+}
+</style>
+
 <template>
   <header>
     <v-container class="d-flex justify-space-between">
@@ -46,24 +67,3 @@ export default Vue.extend({
   },
 })
 </script>
-
-<style lang="scss" scoped>
-header {
-  line-height: 94px;
-  height: 94px;
-  .left-header {
-    ::v-deep {
-      .v-btn {
-        line-height: 42px;
-        height: 42px;
-        a {
-          color: #fff;
-        }
-      }
-    }
-    .auth-link {
-      color: #212121;
-    }
-  }
-}
-</style>
