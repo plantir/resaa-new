50a858b5cc51154210aa1de8ed96aa0c3d6fe781
ali pamenary <pamenary@gmail.com>
2020-05-29 11:58:47 +0430

    add specialties page

---
+++ components/Pages/Specialties/TagList/Tag.vue
@@ -0,0 +1,37 @@
+<style lang="scss" scoped>
+.tag {
+  line-height: 32px;
+  border-radius: 30px;
+  color: #6c6c6c;
+  border: solid 1px #c6c6c6;
+  background-color: #fff;
+  padding: 0 15px;
+  margin-left: 15px;
+  margin-bottom: 15px;
+  font-size: 12px;
+  transition: all 0.25s ease-in;
+  &:hover {
+    background-color: #6c6c6c;
+    color: #fff;
+  }
+}
+</style>
+
+<template>
+  <nuxt-link class="tag" to="">
+    {{ tag.name }}
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Tag extends Vue {
+  @Prop({
+    type: Object,
+    required: true,
+  })
+  readonly tag!: Object
+}
+</script>
---
+++ components/Pages/Specialties/TopSpecialties/CardSpecialtie.vue
@@ -0,0 +1,39 @@
+<style lang="scss" scoped>
+.card-specialtie {
+  .v-card {
+    width: 160px;
+    height: 160px;
+    @include media(sm) {
+      width: 128px;
+      height: 128px;
+    }
+  }
+  h2 {
+    font-size: 14px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
+</style>
+
+<template>
+  <nuxt-link to="/" class="card-specialtie">
+    <v-card class="d-flex flex-column align-center justify-center">
+      <Icon fileName="ic_pregnancy.png" />
+      <h2 class="mt-2">زنان زایمان</h2>
+    </v-card>
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Icon from '@/components/Common/Icon/Icon.vue'
+
+@Component({
+  components: {
+    Icon,
+  },
+})
+export default class CardSpecialtie extends Vue {}
+</script>
