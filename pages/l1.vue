c716246ca820263202795471665d4af8b21fd8e5
sepehr <sepeehrz@gmail.com>
2020-06-08 20:16:18 +0430

    doctor profile

---
+++ pages/l1.vue
@@ -0,0 +1,16 @@
+<style lang="scss" scoped></style>
+<template>
+  <section>
+    <doctorProfileMobile />
+  </section>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop, Watch, Emit, Ref } from 'vue-property-decorator'
+import doctorProfileMobile from '@/components/Pages/doctor/profile/doctorProfile.mobile.vue'
+@Component({
+  components: {
+    doctorProfileMobile,
+  },
+})
+export default class YourComponent extends Vue {}
+</script>
