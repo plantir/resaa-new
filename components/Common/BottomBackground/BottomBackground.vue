0218c4aef73da01f1c7f9e7974bae51396b8f4c0
ali pamenary <pamenary@gmail.com>
2020-06-13 11:28:28 +0430

    add menu mobile

--- components/Common/BottomBackground/BottomBackground.vue
+++ components/Common/BottomBackground/BottomBackground.vue
@@ -6,6 +6,7 @@
   width: 100%;
   background-image: linear-gradient(270deg, #13d1f3, #35d6c1);
   clip-path: ellipse(200% 100% at 50% 100%);
+  z-index: -1;
   @include media(sm) {
     clip-path: ellipse(100% 100% at 50% 100%);
   }
@@ -28,7 +29,7 @@ import { Component, Vue, Prop } from 'vue-property-decorator'
 export default class BottomBackground extends Vue {
   @Prop({
     type: String,
-    default: 400,
+    default: '400',
   })
   readonly height!: String
 }
---
+++ components/Layout/Mobile/HeaderMobile/HeaderMobile.vue
@@ -0,0 +1,46 @@
+<style lang="scss" scoped>
+.main-header {
+  ::v-deep {
+    .theme--dark.v-btn.v-btn--icon {
+      color: $primary-color;
+    }
+  }
+}
+</style>
+
+<template>
+  <v-app-bar class="main-header" color="white" dense dark fixed>
+    <v-app-bar-nav-icon @click="$emit('toggleNavbar')"></v-app-bar-nav-icon>
+
+    <div class="logo">
+      <v-img height="25" width="60" src="/images/logo.png" alt="رسا" contain />
+    </div>
+
+    <v-spacer />
+
+    <v-avatar @click="onAvatar" size="30">
+      <img src="/images/icons/ic_account_circle.svg" />
+    </v-avatar>
+
+    <v-btn outlined dense>
+      <nuxt-link to="/charge">
+        افزایش اعتبار
+      </nuxt-link>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HeaderMobile extends Vue {
+  onAvatar() {
+    if (this.$auth.loggedIn) {
+      this.$router.push('/patient/profile')
+    } else {
+      this.$router.push('/patient/login')
+    }
+  }
+}
+</script>
