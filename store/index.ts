f5fb529119fe75dd88e01e5672f5c2917dee0686
armin <arminkheirkhahan@gmail.com>
2020-07-20 16:21:09 +0430

    remove intro

--- store/index.ts
+++ store/index.ts
@@ -8,7 +8,7 @@ export default class Index extends VuexModule {
   @Action
   async nuxtServerInit(ctx: any) {
     const token = ctx.$auth.getToken('local')
-    const seeIntro = ctx.$auth.$storage.getCookie('seeIntro')
+    // const seeIntro = ctx.$auth.$storage.getCookie('seeIntro')
 
     if (token && ctx.$auth.loggedIn) {
       const profile = ctx.$auth.$storage.getCookie('profile')
@@ -16,8 +16,8 @@ export default class Index extends VuexModule {
     } else {
       // await ctx.$auth.logout()
     }
-    if (!seeIntro && ctx.isMobile) {
-      ctx.redirect('/intro/intro-0')
-    }
+    // if (!seeIntro && ctx.isMobile) {
+    //   ctx.redirect('/intro/intro-0')
+    // }
   }
 }
